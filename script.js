9d13d7ee4a619c3dddb316b82cd512f0d36d9297
Guilherme Dallagnol Vargas <gui_dvargas@hotmail.com>
2021-11-11 21:46:27 -0300

    Mensagens atualizadas de 3 em 3 segundos. Faltando consertar a verificação da atividade/inatividade do usuário

--- script.js
+++ script.js
@@ -1,25 +1,74 @@
 
 let main = document.querySelector("main");
-console.log(main);
+let nomeUsuario = "";
+// console.log(main);
 
-// Chama o servidor com as mensagens do chat UOL
-const promessa = axios.get('https://mock-api.driven.com.br/api/v4/uol/messages');
-promessa.then(processarResposta);
+// let insideInput = document.querySelector(".container input");
+// console.dir(insideInput);
+
+// Pergunta pro servidor se o nome de usuário já existe
+function verificarNome(nomeInserido){
+    let divClass = nomeInserido.parentNode;
+    let inputClass = divClass.querySelector("input");
+    let inputClassValue = inputClass.value;
+    nomeUsuario = {name:`${inputClassValue}`}
+    const reqNome = axios.post('https://mock-api.driven.com.br/api/v4/uol/participants',nomeUsuario)
+    reqNome.then(entrarNaSala);
+    reqNome.catch(nomeIndisponivel);
+}
+
+
+
+function entrarNaSala(){
+    let telaDeEntrada = document.querySelector(".tela-de-entrada");
+    telaDeEntrada.classList.add('hidden');
+    setInterval(verificarStatus,5000);
+    carregarServidor();
+    setInterval(carregarServidor,3000);
+
+}
+
+function verificarStatus(){
+    const reqStatus = axios.post(`https://mock-api.driven.com.br/api/v4/uol/status`, nomeUsuario);
+    reqStatus.then(processarStatus)
+}
+
+
+function carregarServidor(){
+    const promessa = axios.get('https://mock-api.driven.com.br/api/v4/uol/messages');
+    promessa.then(processarResposta);
+}
+
+
+function nomeIndisponivel(){
+    document.querySelector(".nome-invalido").classList.remove(hidden);
+}
+
 
 // Quando a promessa retorna, executa esta função
 function processarResposta(resposta) {
-	console.dir(resposta); // Esse console.log disparará depois
+
+	// console.dir(resposta); 
     const data = resposta.data;
-    console.log(data[0].from);
     carregarMensagens(data);
+
+    const elementoQueQueroQueApareca = document.querySelectorAll(".mensagem");
+    elementoQueQueroQueApareca[elementoQueQueroQueApareca.length-1].scrollIntoView();
+
+}
+
+function processarStatus(resposta){
+    console.dir(resposta);
 }
 
 function carregarMensagens(objeto){
     for (let i = 0; i < objeto.length; i++) {
-        main.innerHTML = main.innerHTML +  `<div class="container ${objeto[i].type}">
-                                            <div class="mensagem">
-                                                <p>${objeto[i].time}  <strong>${objeto[i].from}</strong>  ${objeto[i].text} </p>
+
+        main.innerHTML = main.innerHTML +   `<div class="container ${objeto[i].type}">
+                                                <div class="mensagem">
+                                                    <p>${objeto[i].time}  <strong>${objeto[i].from}</strong>  ${objeto[i].text} </p>
+                                                </div>
                                             </div>
-                                        </div>`     
-    }
+        `   
+    }  
 }
